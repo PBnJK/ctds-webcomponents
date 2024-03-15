92f6b7d3b47b1cd206fac72b83d93d5ad95a352b
Pedro Bui <pedrobuitrago2007yahoo@gmail.com>
2024-03-14 22:16:19 -0300

    work: Funcionalidade como validação de senha e um botão para revelar a senha adicionados

---
+++ scripts/criar.js
@@ -0,0 +1,57 @@
+/**
+ * @file Funções para a página de criação de conta
+ * @author Pedro B.
+ */
+
+
+/**
+ * Valida a senha do usuário
+ */
+function validarCSenha() {
+	if( SENHA_INPUT.value !== CSENHA_INPUT.value ) {
+		CSENHA_INPUT.setCustomValidity("As senhas precisam ser iguais");
+	} else {
+		CSENHA_INPUT.setCustomValidity("");
+	}
+	
+	CSENHA_INPUT.reportValidity();
+}
+
+/**
+ * Revela/esconde a senha digitada
+ */
+function toggleSenha() {
+	const S_INPUT = document.getElementById("inp-b-senha");
+	const EYE     = document.getElementById("eye-pwd-1");
+
+	if( S_INPUT.getAttribute("type") === "password" ) {
+		S_INPUT.setAttribute("type", "text");
+		EYE.setAttribute("src", "assets/eye-slash-solid.svg");
+	} else {
+		S_INPUT.setAttribute("type", "password");
+		EYE.setAttribute("src", "assets/eye-solid.svg");
+	}
+}
+
+/**
+ * Revela/esconde a senha digitada no campo 'confirme a senha'
+ */
+function toggleCSenha() {
+	const CS_INPUT = document.getElementById("inp-b-c-senha");
+	const EYE      = document.getElementById("eye-pwd-2");
+
+	if( CS_INPUT.getAttribute("type") === "password" ) {
+		CS_INPUT.setAttribute("type", "text");
+		EYE.setAttribute("src", "assets/eye-slash-solid.svg");
+	} else {
+		CS_INPUT.setAttribute("type", "password");
+		EYE.setAttribute("src", "assets/eye-solid.svg");
+	}
+}
+
+window.onload = () => {
+	document.getElementById("inp-b-c-senha").onchange = validarCSenha;
+
+	document.getElementById("eye-pwd-1").onclick = toggleSenha;
+	document.getElementById("eye-pwd-2").onclick = toggleCSenha;
+}
