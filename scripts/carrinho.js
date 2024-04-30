43bc0cec5a0af0a2e9576f4f0a77fd0b6e173202
PBnJK <pedrobuitragons@gmail.com>
2024-04-30 14:53:42 -0300

    new stuff

--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -1,11 +1,27 @@
+const TOTAL = document.getElementById('total');
+const CHECKOUT = document.getElementById('checkout');
 const COLLECTION = document.getElementsByClassName('remover');
 
 COLLECTION[0].onclick = function() {
 	document.getElementById('i1').remove();
+
+	if(document.getElementById('i2')) {
+		total.innerText = 'TOTAL: R$2,99';
+	} else {
+		total.innerText = 'TOTAL: R$0,00';
+		CHECKOUT.href = 'javascript:';
+	}
 };
 
 COLLECTION[1].onclick = function() {
 	document.getElementById('i2').remove();
+
+	if(document.getElementById('i1')) {
+		total.innerText = 'TOTAL: R$2,99';
+	} else {
+		total.innerText = 'TOTAL: R$0,00';
+		CHECKOUT.href = 'javascript:';
+	}
 };
 
 const ITEMS = document.getElementsByClassName('item-img');
